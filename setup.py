380972624b4b0fab90729b4a6c660e899488eeb7
Dominick C. Pastore <dominickpastore@dcpx.org>
2019-04-16 13:53:12 -0400

    Builds successfully?

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ setup(
     version="0.1.0dev1",
     description="An module to interface with wpa_supplicant (without DBus)", 
     ext_modules=[Extension('wpa_supplicant',
-        sources=['wpa_supplicant.c', 'wpa_supplicant/wpa_ctrl.c'],
+        sources=[
+            'wpa_supplicant.c',
+            'hostap/src/common/wpa_ctrl.c',
+            'hostap/src/utils/os_unix.c',
+            'hostap/src/utils/common.c',
+            'hostap/src/utils/wpabuf.c',
+            'hostap/src/utils/wpa_debug.c'
+        ],
         define_macros=[
             ('CONFIG_CTRL_IFACE', None),
             ('CONFIG_CTRL_IFACE_UNIX', None)
         ],
-        include_dirs=['hostap/src/common', 'hostap/src/utils']
+        include_dirs=['hostap/src', 'hostap/src/common', 'hostap/src/utils']
     )]
 )
